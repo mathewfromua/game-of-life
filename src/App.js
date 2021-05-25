aad6198c5310506d8e938828b7c5be7ab02b8bdf
Matvii Ruban <mathew_from_ua@icloud.com>
2021-05-25 11:08:51 +0300

    initial gh-pages commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,141 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useRef, useState } from 'react';
+import produce from 'immer';
 
-function App() {
+
+const numRows = 50;
+const numCols = 50;
+
+const operations = [
+    [0, 1],
+    [0, -1],
+    [1, 1],
+    [1, -1],
+    [-1, -1],
+    [-1, 1],
+    [1, 0],
+    [-1, 0]
+];
+
+const generateEmptyGrid = () => {
+    const rows = [];
+    for (let i = 0; i < numRows; i++ ) {
+        rows.push(Array.from(Array(numCols), () => 0));
+    }
+
+    return rows;
+};
+
+
+
+const App = () => {
+    const [drawing, setDrawing] =useState(0);
+
+    const [grid, setGrid] = useState(() => {
+        return generateEmptyGrid();
+    });
+
+  const [running, setRunning] = useState(false);
+
+  const runningRef = useRef(running);
+  runningRef.current = running;
+
+
+  const runSimulation = useCallback(() => {
+      if (!runningRef.current) {
+          return;
+      }
+
+      setGrid(g => {
+          return produce(g, gridCopy => {
+              for (let i = 0; i < numRows; i++) {
+                  for (let k = 0; k < numCols; k++) {
+                      let neighbours = 0;
+                      operations.forEach(([x, y]) => {
+                          const newI = i + x;
+                          const newK = k + y;
+                          if (newI >= 0 && newI < numRows && newK >= 0 && newK < numCols) {
+                              neighbours += g[newI][newK];
+                          }
+                      });
+
+                      if (neighbours < 2 || neighbours > 3) {
+                          gridCopy[i][k] = 0;
+                      } else if (g[i][k] === 0 && neighbours === 3) {
+                          gridCopy[i][k] = 1;
+                      }
+                  }
+
+              }
+          } )
+      })
+
+
+      // simulate
+      setTimeout(runSimulation, 100);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div style={{
+        display: 'grid',
+        gridTemplateColumns: `repeat(${numCols}, 20px)`
+      }}>
+        {grid.map((rows, i) =>
+            rows.map((col, k) =>
+                <div
+                    onMouseEnter={() => {
+                        if (drawing) {
+                            const newGrid = produce(grid, gridCopy => {
+                                gridCopy[i][k] = gridCopy[i][k] ? 0 : 1;
+                            });
+                            setGrid(newGrid)
+                        }
+                    }}
+                    key={`${i}-${k}`}
+                    onClick={() => {
+                      const newGrid = produce(grid, gridCopy => {
+                        gridCopy[i][k] = gridCopy[i][k] ? 0 : 1;
+                      })
+                      setGrid(newGrid)
+                    }}
+                    style={{
+                      width: 20,
+                      height: 20,
+                      backgroundColor: grid[i][k] ? "#227dd9" : undefined,
+                      border: "solid 1px black",
+                    }}
+                />)
+        )}
+      </div>
+      <button onClick={() => {
+          setRunning(!running);
+          if (!running) {
+              runningRef.current = true;
+              runSimulation();
+          }
+      }}
+              style={{width: 100, height: 30, fontSize: 16}}
+      >
+          { running ? 'stop' : 'start' }
+      </button>
+      <button style={{width: 100, height: 30, fontSize: 16}} onClick={() => {
+          setDrawing(!drawing)
+      }}>
+          draw
+      </button>
+        <button style={{width: 100, height: 30, fontSize: 16}} onClick={() => {
+           const rows = [];
+           for (let i = 0; i < numRows; i++) {
+               rows.push(
+                   Array.from(Array(numCols), () => (Math.random() > 0.6 ? 1 : 0))
+               );
+           }
+
+           setGrid(rows);
+        }}>
+            random
+        </button>
+    </>
   );
 }
 
